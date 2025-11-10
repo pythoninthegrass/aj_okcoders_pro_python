81927003f38e6f0f7b072c4cd0cadae925987cb4
AJoshi <ameyasureshjoshi@gmail.com>
2025-11-09 21:28:19 -0600

    Removed an unnecessary print line

--- Class_One_Excercises/Class_One_Challenge.py
+++ Class_One_Excercises/Class_One_Challenge.py
@@ -3,11 +3,9 @@ from datetime import date
 name_of_user = input('Please enter your name: ')
 age_of_user = int(input('Please enter your age: '))
 
+# Get the current year
 date_today = date.today()
-
 current_year = date_today.year
-
-# print('Curr year: ', current_year)
 
 # Get birth year of user. We need to subtract one year from the current year, since the user is going on another year, but has completed a certain number of years already. E.g., if someone is currently 44 years old in 2025, you cannot do 2025 - 44, because that would give you 1981, when the person was actually born in 1980 (1980 + 44 = 2024, not 2025). Thus, we cannot actually take into account the current year, we have to subtract one from it.
 user_birth_year = (current_year - 1) - age_of_user
